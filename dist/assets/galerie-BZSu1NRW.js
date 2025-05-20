f782e590cba411a5caf1ec4100aee3576d8138b7
velovam <Michaela.velova.023@pslib.cz>
2025-05-20 16:59:34 +0200

    dsadsa

---
+++ dist/assets/galerie-BZSu1NRW.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){window.innerWidth<768&&new Swiper(".swiper",{loop:!0,autoplay:{delay:2500},slidesPerView:1.2,spaceBetween:16}),window.innerWidth>=768&&lightGallery(document.getElementById("lightgallery"),{speed:500,plugins:[lgThumbnail]})});
